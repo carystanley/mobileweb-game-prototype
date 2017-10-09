53a2d46774af68fbaa7f9b1cab3010dfea9f5d55
Cary Stanley <cary@yahoo-inc.com>
2017-10-09 14:48:04 -0700

    Refactor drawPanels

--- src/states/battle.js
+++ src/states/battle.js
@@ -38,46 +38,51 @@ BattleState.prototype.update = function () {
 }
 
 BattleState.prototype.draw = function (ctx, res) {
-    var self = this;
     this.background.draw(ctx);
-    var pcs = this.battle.getPlayerCharacters();
-    var pcCount = pcs.length;
-    pcs.forEach(function (pc, idx) {
-        var x = self.game.layout.centeredEvenlySpaced(idx, pcCount, 64);
-        var y = self.game.layout.fromBottom(22);
-        self.drawPanel(ctx, res, 'Cary', pc.hp, x, y);
-    });
+    this.drawPanels(ctx, res);
     this.state.draw(ctx, res);
 }
 
-BattleState.prototype.drawPanel = function (ctx, res, name, num, x, y) {
-    ctx.drawImage(
-        res.statuspanel,
-        0, 0, 60, 35,
-        x - 30, y, 60, 35
-    );
+BattleState.prototype.drawPanels = function (ctx, res) {
+    var layout = this.game.layout;
+    var pcs = this.battle.getPlayerCharacters();
+    var pcCount = pcs.length;
 
-    var font = res.basicfont;
-    var nameLen = font.measureText(name);
-    font.drawText(ctx, name, x - (nameLen/2), y);
+    pcs.forEach(function (pc, idx) {
+        var x = layout.centeredEvenlySpaced(idx, pcCount, 64);
+        var y = layout.fromBottom(22);
 
-    var roll = Math.floor((num % 1) * 8);
-    var rolling = true;
+        var num = pc.hp;
+        var name = 'Cary';
 
-    for (var i = 0; i < 3; i++) {
-        var digit = Math.floor(num % 10);
-        var offset = digit * 8;
-        if (rolling) {
-            offset += roll;
-        }
-        num = num / 10;
         ctx.drawImage(
-            res.odometer,
-            offset * 9, 0, 8, 8,
-            x + 15 - (i * 8), y + 12, 8, 8
+            res.statuspanel,
+            0, 0, 60, 35,
+            x - 30, y, 60, 35
         );
-        rolling = digit === 9 && rolling;
-    }
+
+        var font = res.basicfont;
+        var nameLen = font.measureText(name);
+        font.drawText(ctx, name, x - (nameLen/2), y);
+
+        var roll = Math.floor((num % 1) * 8);
+        var rolling = true;
+
+        for (var i = 0; i < 3; i++) {
+            var digit = Math.floor(num % 10);
+            var offset = digit * 8;
+            if (rolling) {
+                offset += roll;
+            }
+            num = num / 10;
+            ctx.drawImage(
+                res.odometer,
+                offset * 9, 0, 8, 8,
+                x + 15 - (i * 8), y + 12, 8, 8
+            );
+            rolling = digit === 9 && rolling;
+        }
+    });
 }
 
 BattleState.prototype.event = function (type, x, y) {
