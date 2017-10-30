5078902cf70ec7e206e1174c7baf9285dfb8f8d6
Cary Stanley <cary@yahoo-inc.com>
2017-10-30 11:11:55 -0700

    Add 3rd Floor

--- src/states/loading.js
+++ src/states/loading.js
@@ -4,12 +4,16 @@ var BitmapFont = require('../bitmapfont');
 var Map = require('../explore/map');
 var floor1 = require('../../maps/floor1.json');
 var floor2 = require('../../maps/floor2.json');
+var floor3 = require('../../maps/floor3.json');
 var class1 = require('../../maps/class1.json');
 var class2 = require('../../maps/class2.json');
 var class3 = require('../../maps/class3.json');
 var class4 = require('../../maps/class4.json');
 var class5 = require('../../maps/class5.json');
 var class6 = require('../../maps/class6.json');
+var class7 = require('../../maps/class7.json');
+var class8 = require('../../maps/class8.json');
+var class9 = require('../../maps/class9.json');
 
 function loadImage(url, onload) {
     var image = new Image();
@@ -44,12 +48,16 @@ LoadingState.prototype.enter = function () {
         pattern: loadImage('./images/pattern.png', doneCheck),
         floor1: new Map(floor1),
         floor2: new Map(floor2),
+        floor3: new Map(floor3),
         class1: new Map(class1),
         class2: new Map(class2),
         class3: new Map(class3),
         class4: new Map(class4),
         class5: new Map(class5),
         class6: new Map(class6),
+        class7: new Map(class7),
+        class8: new Map(class8),
+        class9: new Map(class9),
         layer0: loadImage('./images/layer0.png', doneCheck),
         layer1: loadImage('./images/layer1.png', doneCheck),
         odometer: loadImage('./images/odometer.png', doneCheck),
