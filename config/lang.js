6198a508d4cf85ece872f0617ac9465d910639dc
Cary Stanley <cary@yahoo-inc.com>
2017-12-07 17:36:34 -0800

    Add Lang Config

---
+++ config/lang.js
@@ -0,0 +1,31 @@
+/* global window */
+/* eslint quotes: "off" */
+
+window.LANG = {
+    '': 'You encountered a #',
+    '': '# suddenly attacked',
+
+    '': '# attacks!',
+    '': '# charged',
+
+    '': '# dodged quickly!',
+    '': '# took mortal damage',
+    '': '# got hurt and collapsed...',
+
+    '': 'It didn\'t work on #!',
+    '': 'It had no visible effect on #!',
+
+    '': '# became tame!',
+    '': '# was taught a lesson!',
+
+    '': 'You Won!',
+    '': 'The battle was lost...',
+
+    '': '# earned # experience points!',
+
+    '': '# took the #',
+
+    '': '',
+    '': '',
+    '': ''
+}
