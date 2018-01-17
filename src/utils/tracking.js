6839b56fa5d60d16ca5632afbc4ca52cde372230
Cary Stanley <cary@yahoo-inc.com>
2018-01-16 16:13:23 -0800

    Initial Tracking Util

---
+++ src/utils/tracking.js
@@ -0,0 +1,32 @@
+var trackingQueue = window._gaq;
+
+var Tracking = {
+    init: function (account) {
+        this.send(['_trackPageview']);
+        if (account) {
+            this.setAccount(account);
+        }
+    },
+
+    setAccount: function(account) {
+        this.send(['_setAccount', account]);
+    },
+
+    trackPage: function(url){
+        this.send(['_trackPageview', url]);
+    },
+
+    trackEvent: function(category, action, label, value){
+        this.send(['_trackEvent', category, action, label, value]);
+    },
+
+    send: function(args) {
+        if (trackingQueue) {
+            trackingQueue.push(args);
+        } else {
+            console.log('Tracking object missing.');
+        }
+    }
+};
+
+module.exports = Tracking;
